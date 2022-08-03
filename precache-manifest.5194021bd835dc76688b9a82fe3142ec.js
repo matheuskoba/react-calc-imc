e9678b4b6595ad9593e8c3f0b1fc7bba9d417bb3
matheuskoba <matheuskoba2021@icloud.com>
2022-08-03 13:57:59 -0300

    Updates

---
+++ precache-manifest.5194021bd835dc76688b9a82fe3142ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f9f85c38b1598966846",
+    "url": "/react-calc-imc/static/css/main.a97fce08.chunk.css"
+  },
+  {
+    "revision": "0f9f85c38b1598966846",
+    "url": "/react-calc-imc/static/js/main.10180c64.chunk.js"
+  },
+  {
+    "revision": "3da6ca0b9802fefa0bfb",
+    "url": "/react-calc-imc/static/js/runtime~main.7e83b5ce.js"
+  },
+  {
+    "revision": "f679d4681ef0c4875c32",
+    "url": "/react-calc-imc/static/js/2.e75ca8e0.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/react-calc-imc/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "35ad4327a4af1b476971c6f6850c0981",
+    "url": "/react-calc-imc/static/media/powered.35ad4327.png"
+  },
+  {
+    "revision": "b8957ff0c2aab88123e09afa10a7ffb3",
+    "url": "/react-calc-imc/index.html"
+  }
+];
